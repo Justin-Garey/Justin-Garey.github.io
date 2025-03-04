06e014aa4b3c68cf9c4a876e5f364dfef533b576
Justin <jgarey02@gmail.com>
2025-03-03 20:48:49 -0500

    Added the base of the webiste and installed Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+export default function App() {
+  return (
+    <div className="bg-stone-900 py-4 px-3 min-h-screen text-white">
+      <div className="w-1/2 mx-auto">
+        <div className="p-3">
+          <h1 className="text-4xl">Justin Garey</h1>
+          <h2 className="text-3xl text-stone-400">Software Engineer</h2>
+        </div>
+        <div className="p-3">
+          <h1 className="text-4xl">About Me</h1>
+          <p className="text-xl mt-3">
+            I have a computer science background blah blah blah blah blah blah
+            blah blah blah blah blah blah blah blah blah blah blah blah blah
+            blah blah blah blah blah blah blah blah blah blah blah blah blah
+            blah blah blah blah blah blah blah blah blah blah blah
+          </p>
+        </div>
+        <div className="p-3">
+          <h1 className="text-4xl">Technical Skills</h1>
+          <p className="text-xl mt-3">
+            List goes here
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
