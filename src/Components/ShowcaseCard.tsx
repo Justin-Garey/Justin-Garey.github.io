42c07f4df7c85815d0fbb18e2ea9acad71ce35d3
Justin Garey <jgarey02@gmail.com>
2025-10-31 19:40:55 -0400

    Cards have been updated and some of the Projects have been added

--- src/Components/ShowcaseCard.tsx
+++ src/Components/ShowcaseCard.tsx
@@ -1,14 +1,16 @@
-
+import '../App.css';
 
 export default function ShowcaseCategory({ category }: { category: any }) {
   return (
-    <div className="">
+    <div className="mb-6 flex flex-wrap gap-6 justify-center">
       {category.items.map((item: any, index: number) => (
-        <a key={index} href={item.link ? item.link : "#"} target="_blank" className="block mb-6 bg-stone-800 p-6 rounded-lg">
-          <h2 className="text-2xl font-semibold mb-2">{item.title}</h2>
-          <p className="mb-2">
-            <div style={{ whiteSpace: "pre-wrap" }}>{item.description}</div>
-          </p>
+        <a key={index} href={item.link ? item.link : "#"} target="_blank" className="card-width">
+          <div className="p-6 bg-cover bg-center bg-eerie-black rounded-lg" style={{ backgroundImage: `url(${item.image_url})` }}>
+            <h2 className="text-2xl font-semibold mb-2">{item.title}</h2>
+            <p className="mb-2">
+              <div>{item.description}</div>
+            </p>
+          </div>
         </a>
       ))}
     </div>
