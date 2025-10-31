b24bd4c496d54eb4aeeb28ad8b36dd2f9ec815fe
Justin Garey <jgarey02@gmail.com>
2025-10-31 19:56:15 -0400

    Updated sizing on cards so they will all be the same height assuming there is not too much text

--- src/Components/ShowcaseCard.tsx
+++ src/Components/ShowcaseCard.tsx
@@ -4,8 +4,8 @@ export default function ShowcaseCategory({ category }: { category: any }) {
   return (
     <div className="mb-6 flex flex-wrap gap-6 justify-center">
       {category.items.map((item: any, index: number) => (
-        <a key={index} href={item.link ? item.link : "#"} target="_blank" className="card-width">
-          <div className="p-6 bg-cover bg-center bg-eerie-black rounded-lg" style={{ backgroundImage: `url(${item.image_url})` }}>
+        <a key={index} href={item.link ? item.link : "#"} target="_blank" className="showcase-card">
+          <div className="p-6 bg-cover bg-center bg-eerie-black rounded-lg h-full" style={{ backgroundImage: `url(${item.image_url})` }}>
             <h2 className="text-2xl font-semibold mb-2">{item.title}</h2>
             <p className="mb-2">
               <div>{item.description}</div>
