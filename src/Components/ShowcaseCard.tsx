e17723317828ce62ef2385bfee575ab842f67f07
Justin Garey <jgarey02@gmail.com>
2025-11-01 12:24:45 -0400

    Updated redirection links to stay in the same browser page

--- src/Components/ShowcaseCard.tsx
+++ src/Components/ShowcaseCard.tsx
@@ -4,7 +4,7 @@ export default function ShowcaseCategory({ category }: { category: any }) {
   return (
     <div className="mb-6 flex flex-wrap gap-6 justify-center">
       {category.items.map((item: any, index: number) => (
-        <a key={index} href={item.link ? item.link : "#"} target="_blank" className="showcase-card">
+        <a key={index} href={item.link ? item.link : "#"} target={item.link && item.link.startsWith("/") ? "_self" : "_blank"} className="showcase-card">
           <div className="p-6 bg-cover bg-center bg-eerie-black rounded-lg h-full" style={{ backgroundImage: `url(${item.image_url})` }}>
             <h2 className="text-2xl font-semibold mb-2">{item.title}</h2>
             <p className="mb-2">
